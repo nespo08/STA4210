95a23dcdf5b884ce39aa7653d9f08c8ef8a9505e
Nicholas Esposito <nesposito@ufl.edu>
2022-10-09 20:03:17 -0400

    Chapter 4 work and HW2

---
+++ Chapter 4/wnba_team_exp_obs_origin_reg.R
@@ -0,0 +1,51 @@
+####################################################################
+####### WNBA 2010-2019
+
+wnba <- read.csv(
+  "https://www.stat.ufl.edu/~winner/data/wnba_20102019_ATS_OU.csv")
+attach(wnba); names(wnba)
+
+tmPrd <- (OU - tmSprd)/2
+summary(tmPrd)
+
+
+##obtain number of games from 2010-2019
+(N.games <- length(OU))
+
+tm.reg1 <- lm(teamPts ~ tmPrd)
+summary(tm.reg1)
+anova(tm.reg1)
+
+# Fit model through the origin
+tm.reg2 <- lm(teamPts ~ tmPrd - 1)
+summary(tm.reg2)
+anova(tm.reg2)
+confint(tm.reg2) # Because n is big, f-test is not valid. 
+# So rely on t-test and/or CI (which is very tight for large n)
+
+tmPrd.grid <- seq(60,110,.01)
+
+win.graph(height=5.5, width=7.0)
+plot(teamPts ~ tmPrd, pch=16, cex=.35, 
+     main="Actual versus Predicted Team Points - WNBA 2010-2019")
+abline(tm.reg1, col="blue", lwd=2)
+lines(tmPrd.grid, predict(tm.reg1, list(tmPrd=tmPrd.grid), int="c")[,2], 
+      col="red", lwd=2)
+lines(tmPrd.grid, predict(tm.reg1, list(tmPrd=tmPrd.grid), int="c")[,3], 
+      col="red", lwd=2)
+lines(tmPrd.grid, predict(tm.reg1, list(tmPrd=tmPrd.grid), int="p")[,2], 
+      col="purple", lwd=2)
+lines(tmPrd.grid, predict(tm.reg1, list(tmPrd=tmPrd.grid), int="p")[,3], 
+      col="purple", lwd=2)
+legend("bottomright", c("Fitted", "CI for Mean", "PI for Game"), lty=1,
+       col=c("blue", "red", "purple"))
+
+tm.reg1 <- lm(teamPts ~ tmPrd)
+summary(tm.reg1)
+anova(tm.reg1)
+
+tm.reg2 <- lm(teamPts ~ -1, offset = tmPrd)
+anova(tm.reg2)
+
+anova(tm.reg2, tm.reg1)
+
